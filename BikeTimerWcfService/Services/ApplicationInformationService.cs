ad80f97e505c61689780f50928f5b3f03e19c9b4
Jacek Dalkowski <jacekdalkowski@gmail.com>
2015-03-15 14:21:38 -0700

    project init

---
+++ BikeTimerWcfService/Services/ApplicationInformationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeTimerWcfService.Services
+{
+    public class ApplicationInformationService : IApplicationInformationService
+    {
+        public string GetServiceInformation()
+        {
+            return "hello world!";
+        }
+    }
+}
---
+++ BikeTimerWcfService/Services/IApplicationInformationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeTimerWcfService.Services
+{
+    [ServiceContract]
+    public interface IApplicationInformationService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="/", ResponseFormat=WebMessageFormat.Json)]
+        string GetServiceInformation();
+    }
+}
